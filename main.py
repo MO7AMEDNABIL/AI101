9363a973cbae023f08eeea917d725cf34551a982
MO7AMEDNABIL <119575409+MO7AMEDNABIL@users.noreply.github.com>
2022-12-01 14:13:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import numpy as np
+import mediapipe as mp
+import time
 
 
 while True: 
@@ -17,6 +19,31 @@ while True:
     cv.imshow('cam',cam)
 
     if cv.waitKey(1) == ord('q'):
+        break
+
+                    
+                    
+src = cv.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+while True:
+
+    p, img = src.read ()
+    img = cv.flip (img, 1)
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv.imshow ("img", img)
+
+    if cv.waitKey (1) == ord ("e"):
         break
 
 
