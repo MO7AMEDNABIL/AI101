15decb633f0b5d651a1f3ab412b96b2b16023ff4
DannyInBlack <danielghobrial11@gmail.com>
2022-12-01 14:07:43 +0200

    next step

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import cv2 as cv
-import numpy as np
+import cv2
+import mediapipe as mp
+import time
+
+src = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 
-while True: 
+while True:
 
-    cam = np.zeros((500,800,3),dtype='uint8') 
+    p, img = src.read ()
+    img = cv2.flip (img, 1)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    cv.imshow('cam',cam)
+    results = hands.process(imgRGB)
 
-    cam[:]= 0,0,255
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-    cv.imshow('cam',cam)
+    cv2.imshow ("img", img)
 
-    cv.putText(cam,"welcome to AI101!",(200,200), cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,255),2 )
-
-    cv.imshow('cam',cam)
-
-    if cv.waitKey(1) == ord('q'):
+    if cv2.waitKey (1) == ord ("e"):
         break
-
-
-
-
-
-
-
-     
